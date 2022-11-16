a50b5f9756c8883a3d4a415899960cef007ac26a
SimonOsorio <soranokuroi@gmail.com>
2022-11-16 04:16:14 -0500

    Fixes

--- Assets/Scripts/Menus/MenuManager.cs
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -3,13 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private Toggle fullscreenToggle;
     private void Start()
     {
         if (PlayerPrefs.GetInt("Fullscreen", 1) == 1) Screen.fullScreen = true;
         else Screen.fullScreen = false;
+        if (fullscreenToggle != null)
+        {
+            fullscreenToggle.isOn = Screen.fullScreen;
+        }
     }
 
     public void SetFullscreen(bool isFullscreen)
